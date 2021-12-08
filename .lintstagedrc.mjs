2b605f1811295d1673b95bc917caa320aceed555
Anthony Hastings <ar.hastings@gmail.com>
2021-12-08 00:02:34 +0000

    Adding stylelint for CSS-in-JS.

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,9 @@
 export default {
-  '**/*.js?(x)': (filenames) =>
-    `yarn lint --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(' --file ')}`,
+  '**/*.{js,jsx,mjs}': (filenames) => {
+    const stylelintCommand = `stylelint ${filenames.join(' ')}`;
+    const nextLintFilenames = filenames.map((file) => file.split(process.cwd())[1]);
+    const nextLintCommand = `yarn lint:js --file ${nextLintFilenames.join(' --file ')}`;
+
+    return [stylelintCommand, nextLintCommand];
+  }
 };

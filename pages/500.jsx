213ea0af57ad962707540c4d80c1563c5907151b
Anthony Hastings <ar.hastings@gmail.com>
2022-03-12 22:14:00 +0000

    adding 404 and 500 pages

---
+++ pages/500.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import { H2 } from '../components/typography';
+
+const PageWrapper = styled('div')`
+  align-items: center;
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const InternalServerError = () => {
+  return (
+    <PageWrapper>
+      <H2>500 | Error Occurred</H2>
+    </PageWrapper>
+  );
+};
+
+export default InternalServerError;

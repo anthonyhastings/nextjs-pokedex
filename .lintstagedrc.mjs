34c8633f1612bdd3cf98626690bbda5c29f7ed3c
Anthony Hastings <ar.hastings@gmail.com>
2022-11-14 20:37:11 +0000

    Upgrades (#20)
    * detaching eslint from prettier
    
    * adding nvmrc

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,9 +1,14 @@
 export default {
   '**/*.{js,jsx,mjs}': (filenames) => {
     const stylelintCommand = `stylelint ${filenames.join(' ')}`;
-    const nextLintFilenames = filenames.map((file) => file.split(process.cwd())[1]);
-    const nextLintCommand = `yarn lint:js --file ${nextLintFilenames.join(' --file ')}`;
+    const prettierCommand = `prettier --check ${filenames.join(' ')}`;
+    const nextLintFilenames = filenames.map(
+      (file) => file.split(process.cwd())[1]
+    );
+    const nextLintCommand = `yarn lint:js --file ${nextLintFilenames.join(
+      ' --file '
+    )}`;
 
-    return [stylelintCommand, nextLintCommand];
-  }
+    return [stylelintCommand, prettierCommand, nextLintCommand];
+  },
 };
